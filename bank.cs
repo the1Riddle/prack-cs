3eab7de8f3eb6d813e6cd229b3a28a0579855542
elvismalsa <elvisotienomboya@gmail.com>
2023-08-22 14:49:36 -0700

    a simple bank account

---
+++ bank.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class BankAccount {
+        private double balance=0;
+        public void Deposit() {
+            Console.WriteLine("input the amount you want to deposit:");
+            double n = Convert.ToDouble(Console.ReadLine());
+            balance += n;
+        }
+        public void Withdraw() {
+            Console.Write("would you wish to withdraw: ");
+            string text = Console.ReadLine();
+            if (text == "yes")
+            {
+            Console.WriteLine("enter the amount you wish to withdraw");
+            double n = Convert.ToDouble(Console.ReadLine());
+            balance -= n;
+            Console.WriteLine("withdrawal success");
+            }
+        }
+        public double GetBalance() {
+            Console.Write("your account balance is: ");
+            return balance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount b = new BankAccount();
+            b.Deposit();
+            b.Withdraw();
+            Console.WriteLine(b.GetBalance());
+        }
+    }
+}
