859e9cf8948f7490cda5ac0eaeda2094857820f6
elvismalsa <elvisotienomboya@gmail.com>
2023-08-27 23:40:06 -0700

    new updates to make it more amazing

--- bank.cs
+++ bank.cs
@@ -4,46 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SoloLearn
+namespace TheOffsets
 {
-    class BankAccount {
-        private double balance=0;
-        public void Deposit() {
-            Console.WriteLine("input the amount you want to deposit:");
-            double n = Convert.ToDouble(Console.ReadLine());
-            balance += n;
-        }
-        public void Withdraw() {
-            Console.Write("would you wish to withdraw: ");
-            string text = Console.ReadLine();
-            if (text == "yes")
-            {
-            Console.WriteLine("enter the amount you wish to withdraw");
-            double n = Convert.ToDouble(Console.ReadLine());
-	    if (n > balance)
-	    {
-		    Console.WriteLine("Oops! You can\'t withdraw that amount");
-	    }
-	    else
-	    {
-            balance -= n;
-            Console.WriteLine("withdrawal success");
-	    } 
-            }
-        }
-        public double GetBalance() {
-            Console.Write("your account balance is: ");
-            return balance;
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BankAccount b = new BankAccount();
-            b.Deposit();
-            b.Withdraw();
-            Console.WriteLine(b.GetBalance());
-        }
-    }
+	class BankAccount
+	{
+		private double balance = 0;
+
+		public void Deposit()
+		{
+			Console.WriteLine("Enter the amount you want to deposit:");
+			double amount = GetValidAmountInput();
+			balance += amount;
+			Console.WriteLine($"Deposit of {amount:C} successful");
+		}
+
+		public void Withdraw()
+		{
+			Console.Write("Do you wish to withdraw (yes/no): ");
+			string response = Console.ReadLine().ToLower();
+			if (response == "yes")
+			{
+				Console.WriteLine("Enter the amount you wish to withdraw:");
+				double amount = GetValidAmountInput();
+				if (amount > balance)
+				{
+					Console.WriteLine("...\nOops! You can't withdraw more than your balance");
+				}
+				else
+				{
+					balance -= amount;
+					Console.WriteLine($"Withdrawal of {amount:C} successful");
+				}
+			}
+		}
+
+		public double GetBalance()
+		{
+			return balance;
+		}
+
+		private double GetValidAmountInput()
+		{
+			double amount;
+			while (!double.TryParse(Console.ReadLine(), out amount) || amount <= 0)
+			{
+				Console.Write("Invalid input.\nPlease enter a valid positive amount: ");
+			}
+			return amount;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BankAccount account = new BankAccount();
+			account.Deposit();
+			account.Withdraw();
+			Console.WriteLine("...\nThankyou for using our Services");
+			Console.WriteLine($"Your account balance is: {account.GetBalance():C}\n...");
+		}
+	}
 }
