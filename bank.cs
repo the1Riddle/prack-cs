65fb8433f9d0a3cab22f9d384967bad8c547992b
the1Riddle <elvisotienomboya@gmail.com>
2023-11-04 18:13:16 +0300

    implementing the login part

--- bank.cs
+++ bank.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheOffsets
+namespace the1Riddle
 {
 	class BankAccount
 	{
@@ -12,19 +12,19 @@ namespace TheOffsets
 
 		public void Deposit()
 		{
-			Console.WriteLine("Enter the amount you want to deposit:");
+			Console.Write("Enter the amount you want to deposit:");
 			double amount = GetValidAmountInput();
 			balance += amount;
-			Console.WriteLine($"Deposit of {amount:C} successful");
+			Console.WriteLine($"\n          _____Deposit of {amount:C} successful_____\n");
 		}
 
 		public void Withdraw()
 		{
 			Console.Write("Do you wish to withdraw (yes/no): ");
 			string response = Console.ReadLine().ToLower();
-			if (response == "yes")
+			if (response == "yes" || response == "y")
 			{
-				Console.WriteLine("Enter the amount you wish to withdraw:");
+				Console.WriteLine("Enter the amount you wish to withdraw: ");
 				double amount = GetValidAmountInput();
 				if (amount > balance)
 				{
@@ -54,15 +54,61 @@ namespace TheOffsets
 		}
 	}
 
+	class User
+	{
+	    private string name;
+	    private string password;
+	    private int loginAttempts = 0;
+
+	    public void CreateAccount()
+	    {
+	        Console.WriteLine("*************** Welcome to the Platform ***************");
+	        Console.WriteLine("\n ...To continue, please create an account \n");
+	        Console.Write("Enter your Name: ");
+	        name = Console.ReadLine();
+	        Console.Write("Enter your password: ");
+	        password = (Console.ReadLine());
+	        Console.WriteLine("\n \n               _____Account Creation Success_____");
+	        Console.WriteLine($"\n \n*************** Welcome to your New account {name} ***************\n");
+	    }
+
+	    public bool Authenticate()
+	    {
+	        while (loginAttempts < 3)
+	        {
+	            Console.WriteLine("To continue, please enter your password: \n");
+	            string inputPassword = (Console.ReadLine());
+	            if (inputPassword == password)
+	            {
+	                Console.WriteLine("            _____Login successful_____\n");
+	                return true;
+	            }
+	            else
+	            {
+	                Console.WriteLine("            ....Account login Failure");
+	                loginAttempts++;
+	            }
+	        }
+	        Console.WriteLine("\n...Authentication failed. Come back after 30 minutes and try again");
+	        Console.WriteLine("\n \n**************** Account Suspended ***************\n");
+	        return false;
+	    }
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			User user = new User();
+			user.CreateAccount();
 			BankAccount account = new BankAccount();
 			account.Deposit();
-			account.Withdraw();
-			Console.WriteLine("...\nThankyou for using our Services");
-			Console.WriteLine($"Your account balance is: {account.GetBalance():C}\n...");
+			if (user.Authenticate())
+			{
+				account.Withdraw();
+				Console.WriteLine($"Your account balance is: {account.GetBalance():C}");
+				Console.WriteLine("\n\n         ______Thankyou for using our Services______");
+			}
 		}
 	}
 }
