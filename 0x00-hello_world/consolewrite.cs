b33bc9b9c5243d81e52e8654d912f335dd8174c9
the1Riddle <elvisotienomboya@gmail.com>
2024-02-18 21:42:29 +0300

    a program that prints an exact word

---
+++ 0x00-hello_world/consolewrite.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyConsoleWrite
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("with proper grammar, but the outcome is a piece of art,");
+		}
+	}
+}
